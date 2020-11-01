3221502daf271eff189ca0fdccf13ebf87297d2a
leo <leo.ertuna@gmail.com>
2020-11-02 01:37:09 +0300

    Cloud Eureka

---
+++ cloud-eureka/src/main/java/com/example/microservices/cloud_eureka/CloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.cloud_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer @SpringBootApplication
+public class CloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
