fd45576d9e56fbdf24642bf5715324d7c80ae4dc
leo <leo.ertuna@gmail.com>
2020-11-02 01:52:42 +0300

    Message protocol

---
+++ message-protocol/src/main/java/com/example/microservices/message_protocol/PingMO.java
@@ -0,0 +1,11 @@
+package com.example.microservices.message_protocol;
+
+import com.example.microservices.MessageObject;
+import lombok.*;
+
+@Getter @AllArgsConstructor @NoArgsConstructor @ToString @EqualsAndHashCode
+public class PingMO implements MessageObject {
+    private String id;
+    private boolean success;
+    private String serviceId;
+}
